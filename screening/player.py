a3cd7d51d93976c36d1585770f0c4485eda02bb3
George <GeorgeGreen.QA@yandex.ru>
2022-12-14 13:58:49 +0400

    _

--- screening/player.py
+++ screening/player.py
@@ -1,11 +1,5 @@
 from helpers.helpers import *
 import pygame
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 
 class Player(pygame.sprite.Sprite):
