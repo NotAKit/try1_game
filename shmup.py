9f483007c2114a82654d654c37d54cbf1c8aadf2
George <GeorgeGreen.QA@yandex.ru>
2022-12-14 13:53:44 +0400

    shmup changes

--- shmup.py
+++ shmup.py
@@ -1,17 +1,8 @@
+from screening.player import Player
+from helpers.helpers import *
 # Pygame шаблон - скелет для нового проекта Pygame
 import pygame
 import random
-
-WIDTH = 1080
-HEIGHT = 720
-FPS = 30
-
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 # Создаем игру и окно
 pygame.init()
@@ -19,45 +10,6 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 10))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.centery = HEIGHT / 2
-        self.speedx = 0
-        self.speedy = 0
-
-
-    def update(self):
-        self.speedx = 0
-        self.speedy = 0
-        keystate = pygame.key.get_pressed()
-        # движение влево и вправо
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -8
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
-        self.rect.x += self.speedx
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-        #     движение вверх и вниз
-        if keystate[pygame.K_UP]:
-            self.speedy = -8
-        if keystate[pygame.K_DOWN]:
-            self.speedy = 8
-        self.rect.y += self.speedy
-        if self.rect.top > HEIGHT:
-            self.rect.top = HEIGHT
-        if self.rect.bottom < 0:
-            self.rect.bottom = 0
-
-
 
 all_sprites = pygame.sprite.Group()
 player = Player()
@@ -73,6 +25,7 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+
 
     # Обновление
     all_sprites.update()
